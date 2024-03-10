322c4bace30205d781a1b65dc55e6abd84c8dd68
emanuel <emanuel@laptopdf>
2024-03-10 15:04:40 -0500

    Se crearon las imagenes para poner en la interfaz principal

---
+++ src/vista/JFramePrincipal.java
@@ -0,0 +1,16 @@
+package vista;
+
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+
+public class JFramePrincipal extends JFrame {
+	public JFramePrincipal() {
+		inicializarComponentes();
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	}
+
+	private void inicializarComponentes() {
+	}
+
+	private JLabel LOGO;
+}
